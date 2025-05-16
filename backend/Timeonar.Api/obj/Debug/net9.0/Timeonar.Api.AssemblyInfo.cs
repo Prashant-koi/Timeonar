bb79a881ec61b0cae71752825a44ff1f4fb2aec8
Prashant-koi <koiralaprashanta10@gmail.com>
2025-05-16 18:10:49 -0400

    build

--- backend/Timeonar.Api/obj/Debug/net9.0/Timeonar.Api.AssemblyInfo.cs
+++ backend/Timeonar.Api/obj/Debug/net9.0/Timeonar.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Timeonar.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7be64a5419c186f536a9c4e44c136115c5a51075")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b39dbd9f47e960f944efe62d1e43200102ab24ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("Timeonar.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Timeonar.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
